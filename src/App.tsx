0b3f860eaaadcf926831e128a1335a468d855388
ssofianne <akovenkosofia101@gmail.com>
2024-12-04 05:46:36 +0300

    add tauri

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,28 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import WorksPage from './pages/WorksPage/WorksPage'
 import {WorkDetailPage} from './pages/WorkDetailPage/WorkDetailPage'
+import { useEffect } from 'react';
+
+// const { invoke } = (window as any).__TAURI__.tauri;
 
 function App(){
+
+  useEffect(() => {
+    if (window.TAURI) {
+      const { invoke } = window.TAURI.tauri;
+
+      invoke('tauri', { cmd: 'create' })
+        .then((response: any) => console.log(response))
+        .catch((error: any) => console.log(error));
+
+      return () => {
+        invoke('tauri', { cmd: 'close' })
+          .then((response: any) => console.log(response))
+          .catch((error: any) => console.log(error));
+      };
+    }
+  }, []);
+
   return(
 
     <Routes>
